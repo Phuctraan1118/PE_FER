bd278dccb1795a7677c4acb21bd3a167e68e27e6
Tran Thien Phuc <105199129+Phuctraan1118@users.noreply.github.com>
2022-07-19 14:57:18 +0700

    Exercise 30_Single Page Applications

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -44,7 +44,7 @@ function RenderCard({item, isLoading, errMess}) {
     }
     
     const Menu = (props) => {
-        // const menu = props.dishes.map((dish) => {
+    
         const menu = props.dishes.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1"  key={dish.id}>
